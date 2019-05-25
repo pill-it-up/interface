f1153038eb6bd3036de14c17663b74e1d7dfb108
Daniel Nery <danielnso97@gmail.com>
2019-05-24 23:43:49 -0300

    Add splash screen

--- js/script.js
+++ js/script.js
@@ -2,10 +2,38 @@ var app = new Vue({
     el: '#app',
     data: {
         title: 'Pill It Up!',
+        started: false,
+        activateButtons: false,
     },
     methods: {
         updatePill: () => {
 
         },
+
+        startApp: function () {
+            if (this.started) {
+                this.activateButtons = false;
+
+                const buttons = this.$el.querySelector('.buttons');
+                const startFalse = () => {
+                    this.started = false;
+                    buttons.removeEventListener('transitionend', startFalse);
+                }
+
+                buttons.addEventListener('transitionend', startFalse);
+
+                return;
+            }
+
+            this.started = true;
+
+            const transition = this.$el.querySelector('.logo');
+            const buttonsTrue = () => {
+                this.activateButtons = true;
+                transition.removeEventListener('transitionend', buttonsTrue);
+            }
+
+            transition.addEventListener('transitionend', buttonsTrue);
+        }
     },
 })
